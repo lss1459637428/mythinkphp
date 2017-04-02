5824f9fb09d41f7ee8a21f19ace7d14004e847ac
lss1459637428 <1459637428@qq.com>
2017-04-02 18:07:03 +0800

    加载core

---
+++ core/Bootstrap.php
@@ -0,0 +1,12 @@
+<?php namespace  core;
+/**
+ * Created by PhpStorm.
+ * User: qx
+ * Date: 2017/4/2
+ * Time: 17:41
+ */
+class Bootstrap{
+    public static function run(){
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qx
+ * Date: 2017/4/2
+ * Time: 17:41
+ */
+include "vendor/autoload.php";
+core\Bootstrap::run();
